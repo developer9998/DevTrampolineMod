aa078639c7a10d65e776027b71d99286084293dd
dev <81720436+developer9998@users.noreply.github.com>
2023-11-13 02:12:56 -0330

    Bump version

--- DevTrampolineMod/Constants.cs
+++ DevTrampolineMod/Constants.cs
@@ -4,7 +4,7 @@ namespace DevTrampolineMod
     public class Constants
     {
         // General Info
-        public const string Guid = "dev.trampolinemod", Name = "DevTrampolineMod", Version = "1.1.0";
+        public const string Guid = "dev.trampolinemod", Name = "DevTrampolineMod", Version = "1.1.1";
 
         // Logic
         public const int TrampolineLimit = 30;
